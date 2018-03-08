16f892927a241eea7dec3ff18b84ec4b6d63045d
Kyushik <kyushikmin@gmail.com>
2018-03-08 11:10:56 +0900

    Create folder for Q version of omok

---
+++ 2. omok/Q/Selfplay_demo.py
@@ -0,0 +1,68 @@
+# Deep Q-Network Algorithm
+
+# Import modules
+import pygame
+import random
+import numpy as np
+import time
+
+import env_small
+import env_regular
+import env_large as game
+
+class self_demo:
+	def __init__(self):
+
+		# Game Information
+		self.game_name = game.ReturnName()
+
+		# Get parameters
+		self.Num_action = game.Return_Num_Action()
+
+		# Initialize Parameters
+		self.step = 1
+		self.score = 0
+		self.episode = 0
+
+		# Turn = 0: Black, 1: White
+		self.turn = 0
+
+		self.Num_spot = int(np.sqrt(self.Num_action))
+		self.gameboard = np.zeros([self.Num_spot, self.Num_spot])
+		self.state = np.zeros([self.Num_spot, self.Num_spot, 17])
+		self.check_valid_pos = 0
+		self.win_index = 0
+
+	def main(self):
+		# Define game state
+		game_state = game.GameState()
+		action = 0
+
+		# Game Loop
+		while True:
+			# Select action
+			action = np.zeros([self.Num_action])
+
+			# Find legal moves
+			count_move = 0
+			legal_index = []
+			for i in range(self.Num_spot):
+				for j in range(self.Num_spot):
+					# Append legal move index into list
+					if self.gameboard[i,j] == 0:
+						legal_index.append(count_move)
+					count_move += 1
+
+			# Randomly take action among legal actions
+			if len(legal_index) > 0:
+				action[random.choice(legal_index)] = 1
+
+			# Take action and get info. for update
+			self.gameboard, self.state, self.check_valid_pos, self.win_index, self.turn = game_state.step(action)
+
+			# Delay for visualization
+			# time.sleep(0.01)
+
+if __name__ == '__main__':
+	agent = self_demo()
+	agent.main()
---
+++ 2. omok/Q/mcts_vs.py
@@ -0,0 +1,212 @@
+from utils import valid_actions, check_win
+from copy import deepcopy
+import numpy as np
+import random
+import time
+
+import env_small as game
+import env_regular
+import env_large
+
+class MCTS:
+    def __init__(self, win_mark):
+        # Get parameters
+        self.win_mark = win_mark
+
+    def selection(self, tree):
+        node_id = (0,)
+
+        while True:
+            num_child = len(tree[node_id]['child'])
+            # Check if current node is leaf node
+            if num_child == 0:
+                return node_id
+            else:
+                max_value = -100
+                leaf_id = node_id
+                for i in range(num_child):
+                    action = tree[leaf_id]['child'][i]
+                    child_id = leaf_id + (action,)
+                    w = tree[child_id]['w']
+                    n = tree[child_id]['n']
+                    total_n = tree[tree[child_id]['parent']]['n']
+
+                    # for unvisited child, cannot compute u value
+                    # so make n to be very small number
+                    if n == 0:
+                        q = w / 0.0001
+                        u = 10 * np.sqrt(2 * np.log(total_n) / 0.0001)
+                    else:
+                        q = w / n
+                        u = 10 * np.sqrt(2 * np.log(total_n) / n)
+
+                    if q + u > max_value:
+                        max_value = q + u
+                        node_id = child_id
+
+    def expansion(self, tree, leaf_id):
+        leaf_state = deepcopy(tree[leaf_id]['state'])
+        is_terminal = check_win(leaf_state, self.win_mark)
+        actions = valid_actions(leaf_state)
+        expand_thres = 20
+
+        if leaf_id == (0,) or tree[leaf_id]['n'] > expand_thres:
+            is_expand = True
+        else:
+            is_expand = False
+
+        if is_terminal == 0 and is_expand:
+            # expansion for every possible actions
+            childs = []
+            for action in actions:
+                state = deepcopy(tree[leaf_id]['state'])
+                action_index = action[1]
+                current_player = tree[leaf_id]['player']
+
+                if current_player == 0:
+                    next_turn = 1
+                    state[action[0]] = 1
+                else:
+                    next_turn = 0
+                    state[action[0]] = -1
+
+                child_id = leaf_id + (action_index, )
+                childs.append(child_id)
+                tree[child_id] = {'state': state,
+                                  'player': next_turn,
+                                  'child': [],
+                                  'parent': leaf_id,
+                                  'n': 0,
+                                  'w': 0,
+                                  'q': 0}
+
+                tree[leaf_id]['child'].append(action_index)
+
+            child_id = random.sample(childs, 1)
+            return tree, child_id[0]
+        else:
+            # If leaf node is terminal state,
+            # just return MCTS tree
+            return tree, leaf_id
+
+    def simulation(self, tree, child_id):
+        state = deepcopy(tree[child_id]['state'])
+        player = deepcopy(tree[child_id]['player'])
+
+        while True:
+            win = check_win(state, self.win_mark)
+            if win != 0:
+                return win
+            else:
+                actions = valid_actions(state)
+                action = random.choice(actions)
+                if player == 0:
+                    player = 1
+                    state[action[0]] = 1
+                else:
+                    player = 0
+                    state[action[0]] = -1
+
+    def backup(self, tree, child_id, sim_result):
+        player = deepcopy(tree[(0,)]['player'])
+        node_id = child_id
+
+        # sim_result: 1 = O win, 2 = X win, 3 = Draw
+        if sim_result == 3:
+            value = 1
+        elif sim_result - 1 == player:
+            value = 1
+        else:
+            value = 0
+
+        while True:
+            tree[node_id]['n'] += 1
+            tree[node_id]['w'] += value
+            tree[node_id]['q'] = tree[node_id]['w'] / tree[node_id]['n']
+
+            parent_id = tree[node_id]['parent']
+            if parent_id == (0,):
+                tree[parent_id]['n'] += 1
+                return tree
+            else:
+                node_id = parent_id
+
+
+if __name__ == '__main__':
+    # tic-tac-toe game environment
+    env = game.GameState()
+    state_size, win_mark = game.Return_BoardParams()
+    agent = MCTS(win_mark)
+
+    board_shape = [state_size, state_size]
+    game_board = np.zeros(board_shape)
+
+    do_mcts = False
+    num_mcts = 100
+    # 0: O, 1: X
+    turn = 0
+    ai_turn = 1
+
+    while True:
+        # Select action
+        action = np.zeros([state_size, state_size])
+
+        # MCTS
+        if do_mcts:
+        	# Initialize Tree
+        	root_id = (0,)
+        	tree = {root_id: {'state': game_board,
+        	                  'player': turn,
+        	                  'child': [],
+        	                  'parent': None,
+        	                  'n': 0,
+        	                  'w': None,
+        	                  'q': None}}
+
+        	for i in range(num_mcts):
+        	    # step 1: selection
+        	    leaf_id = agent.selection(tree)
+        	    # step 2: expansion
+        	    tree, child_id = agent.expansion(tree, leaf_id)
+        	    # step 3: simulation
+        	    sim_result = agent.simulation(tree, child_id)
+        	    # step 4: backup
+        	    tree = agent.backup(tree, child_id, sim_result)
+
+        	print('-------- current state --------')
+        	print(tree[(0,)]['state'])
+        	q_list = {}
+        	actions = tree[(0,)]['child']
+        	for i in actions:
+        	    q_list[(0, i)] = tree[(0, i)]['q']
+
+        	# Find Max Action
+        	max_action = max(q_list, key=q_list.get)[1]
+        	print('max action: ' + str(max_action + 1))
+        	do_mcts = False
+
+        	action = np.zeros([state_size * state_size])
+        	action[max_action] = 1
+        	################################################################################################
+
+        # Take action and get info. for update
+        game_board, state, check_valid_pos, win_index, turn = env.step(action)
+
+        # If one move is done
+        if turn == ai_turn:
+        	do_mcts = True
+
+        # If game is finished
+        if win_index != 0:
+            game_board = np.zeros([state_size, state_size])
+
+            # Human wins!
+            if turn == ai_turn:
+                ai_turn = 0
+                do_mcts = True
+            else:
+                ai_turn = 1
+                do_mcts = False
+
+        # Delay for visualization
+        time.sleep(0.01)
