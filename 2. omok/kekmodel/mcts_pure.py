e368571b92e89810a4dd0d00455719692a839d67
kekmodel <kekmodel@gmail.com>
2018-03-12 14:39:20 +0900

    Test pure mcts

--- 2. omok/kekmodel/mcts_pure.py
+++ 2. omok/kekmodel/mcts_pure.py
@@ -33,7 +33,7 @@ class MCTS:
         self.no_legal_move = None
         self.ucb = None
 
-        # reset member
+        # used for backup
         self.key_memory = None
         self.action_memory = None
 
@@ -51,12 +51,14 @@ class MCTS:
     def get_action(self, state):
         self.root = state.copy()
         self._simulation(state)
-        # root board
+        # init root board after simulatons
         self.board = self.root.reshape(3, BOARD_SIZE**2)
         board_fill = self.board[CURRENT] + self.board[OPPONENT]
         self.legal_move = np.argwhere(board_fill == 0).flatten()
         self.no_legal_move = np.argwhere(board_fill != 0).flatten()
+        # root state's key
         root_key = xxh64(self.root.tostring()).hexdigest()
+        # argmax Q
         action = self._selection(root_key, c_ucb=0)
         print(self.ucb.reshape(BOARD_SIZE, BOARD_SIZE).round(decimals=4))
         return action
@@ -67,31 +69,38 @@ class MCTS:
         for sim in range(SIMULATIONS):
             if (sim + 1) % (160) == 0:
                 print('.', end='', flush=True)
+            # reset state
             self.state = self.env_simul.reset(state)
             done = False
             n_selection = 0
             n_expansion = 0
             while not done:
+                # init board
                 self.board = self.state.reshape(3, BOARD_SIZE**2)
                 board_fill = self.board[CURRENT] + self.board[OPPONENT]
                 self.legal_move = np.argwhere(board_fill == 0).flatten()
                 self.no_legal_move = np.argwhere(board_fill != 0).flatten()
                 key = xxh64(self.state.tostring()).hexdigest()
+                # search my tree
                 if key in self.tree:
+                    # selection
                     action = self._selection(key, c_ucb=1)
                     self.action_memory.appendleft(action)
                     self.key_memory.appendleft(key)
                     n_selection += 1
                 else:
                     if n_expansion == 0:
+                        # expansion
                         action = self._expansion(key)
                         self.action_memory.appendleft(action)
                         self.key_memory.appendleft(key)
                         n_expansion += 1
                     else:
+                        # rollout
                         action = random.choice(self.legal_move)
                 self.state, reward, done = self.env_simul.step(action)
             if done:
+                # backup & reset memory
                 self._backup(reward, n_selection + n_expansion)
                 self._reset()
         finish = round(time.time() - start)
@@ -99,16 +108,20 @@ class MCTS:
 
     def _selection(self, key, c_ucb):
         edges = self.tree[key]
+        # get ucb
         ucb = self._ucb(edges, c_ucb)
         self.ucb = ucb
         if self.board[COLOR][0] == WHITE:
+            # black's choice
             action = np.argwhere(ucb == ucb.max()).flatten()
         else:
+            # white's choice
             action = np.argwhere(ucb == ucb.min()).flatten()
         action = action[random.choice(len(action))]
         return action
 
     def _expansion(self, key):
+        # only select once for rollout
         action = self._selection(key, c_ucb=1)
         return action
 
@@ -117,6 +130,7 @@ class MCTS:
         ucb = np.zeros((BOARD_SIZE**2), 'float')
         for i in range(BOARD_SIZE**2):
             total_N += edges[i][N]
+        # black's ucb
         if self.board[COLOR][0] == WHITE:
             for move in self.legal_move:
                 if edges[move][N] != 0:
@@ -126,6 +140,7 @@ class MCTS:
                     ucb[move] = np.inf
             for move in self.no_legal_move:
                 ucb[move] = -np.inf
+        # white's ucb
         else:
             for move in self.legal_move:
                 if edges[move][N] != 0:
@@ -138,6 +153,8 @@ class MCTS:
         return ucb
 
     def _backup(self, reward, steps):
+        # steps = num_selection + num_expansion
+        # update edges in my tree
         for i in range(steps):
             edges = self.tree[self.key_memory[i]]
             action = self.action_memory[i]
@@ -149,9 +166,9 @@ def main():
     env = OmokEnv()
     mcts = MCTS(SIMULATIONS)
     result = {'Black': 0, 'White': 0, 'Draw': 0}
-    step_game = 0
     for game in range(GAMES):
         print('#########  GAME: {}  #########\n'.format(game + 1))
+        # reset state
         state = env.reset()
         done = False
         while not done:
@@ -159,7 +176,6 @@ def main():
             # start simulations
             action = mcts.get_action(state)
             state, z, done = env.step(action)
-            step_game += 1
         if done:
             if z == 1:
                 result['Black'] += 1
